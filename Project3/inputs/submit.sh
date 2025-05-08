7f8a42fea20db1a284d4a7d40b0bd985db02f1d2
shawk17 <kadengshaw@gmail.com>
2025-05-08 13:04:39 -0600

    Update submit.sh

--- Project3/inputs/submit.sh
+++ Project3/inputs/submit.sh
@@ -10,7 +10,7 @@
 
 module load gromacs+plumed/2024.5/openmpi/4.1.3/gcc/12.1.0
 export OMP_NUM_THREADS=4
-export GMXLIB=$HOME/GMXLIB
+# export GMXLIB=$HOME/GMXLIB
 
 bash gro-step min
 bash gro-step min2
